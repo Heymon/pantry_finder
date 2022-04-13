0ffaffa9c0cf2dabcc9f363cc773dd8db4044f0c
Heymon <rcavalleiro@gmail.com>
2022-04-13 12:55:12 -0700

    set up schemas for DB

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+from django.core.validators import RegexValidator
 # Create your models here.
+
+class Pantry(models.Model):
+
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=50, unique=True)
+    email=models.EmailField(
+        validators=[
+            RegexValidator(
+                    regex='^\w.{1,64}@\w.{1,253}\.\w*$',
+                    message='EX: address@domain.com',
+                    code="Invalid Email Format"
+                    )])
+    phone=models.CharField(
+        validators=[
+            RegexValidator(
+                regex='^(\+?\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$',
+                message='EX: +# ###-###-####',
+                code="Invalid Phone Format"
+                )])
+    description=models.TextField(max_length=1000, blank=True)
+
+class Location(models.Model):
+    pantry=models.OneToOneField(Pantry, on_delete=models.CASCADE)
+    address=models.CharField(max_length=100)
+    lat=models.FloatField()
+    lng=models.FloatField()
+    google_id=models.CharField()
+
+class Item(models.Model):
+    pantry=models.ForeignKey(Pantry, on_delete=models.CASCADE)
+    name=models.CharField(max_length=50);
+    best_by=models.models.DateField("Best By", auto_now=False, auto_now_add=False, blank=True)
+    quantity=models.IntegerField(default=1)
+    perishable=models.BooleanField(default=True)
+    refrigeration=models.BooleanField(default=False)
+    has_item=models.BooleanField(default=True)
+    keywords=models.TextField(max_length=300, blank=True)
+    # keywords=ArrayField(base_field=models.CharField(max_length=10)
+
+
