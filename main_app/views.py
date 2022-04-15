aec2e4f8607b264da158ce7c09075bbb1ff541b3
Heymon <rcavalleiro@gmail.com>
2022-04-15 00:03:55 -0700

    add static css &  base templating

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from user_app.forms import User_Form
+from .forms import Pantry_Form, Location_Form
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    user_form = User_Form
+    pantry_form = Pantry_Form
+    location_form = Location_Form
+    context = {'user_form': user_form, 'pantry_form': pantry_form, 'location_form': location_form}
+    return render(request,'home.html', context)
