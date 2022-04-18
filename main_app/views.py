0fae4030776d78c66d62cdad5aa685e1c189e276
Heymon <rcavalleiro@gmail.com>
2022-04-18 13:06:07 -0700

    add seed pantries & html/styling to home and base

--- main_app/views.py
+++ main_app/views.py
@@ -7,5 +7,88 @@ def home(request):
     user_form = User_Form
     pantry_form = Pantry_Form
     location_form = Location_Form
-    context = {'user_form': user_form, 'pantry_form': pantry_form, 'location_form': location_form}
+    context = {'pantries': pantries,'user_form': user_form, 'pantry_form': pantry_form, 'location_form': location_form}
     return render(request,'home.html', context)
+
+class Location:
+
+    def  __init__(self, address, lat, lng ):
+        self.address = address
+        self.lat = lat
+        self.lng = lng
+
+class Pantry:
+
+    def __init__(self, id, title, location, contact, description ):
+        self.id = id
+        self.title = title
+        self.location = location
+        self.contact = contact
+        self.description = description
+
+pantries = [
+    Pantry(#
+        "1D",#id
+        "San Francisco", #charField
+        Location("San Francisco, CA, USA", 37.77514760291072, -122.41941013558197),#charField, integerField
+        "415-975-4767",#charField 
+        "Only has ham"#textField
+        ),
+    Pantry(
+        "2D",
+        "Ocean Beach", 
+        Location("Ocean Beach, San Francisco, CA, USA", 37.75971813007669, -122.51073936441803),
+        "415-975-4767",
+        "NO HAMS ALLOWED"
+        ),
+    Pantry(
+        "3D",
+        "Community Fridge and Cabinet", 
+        Location("20324 Forest Ave, Castro Valley, CA 94546", 37.69752069616992, -122.06889211536156),
+        "NOT AVAILABLE",
+        "Community Organized"
+        ),
+    Pantry(#
+        "1D",#id
+        "San Francisco", #charField
+        Location("San Francisco, CA, USA", 37.77514760291072, -122.41941013558197),#charField, integerField
+        "415-975-4767",#charField 
+        "Only has ham"#textField
+        ),
+    Pantry(
+        "2D",
+        "Ocean Beach", 
+        Location("Ocean Beach, San Francisco, CA, USA", 37.75971813007669, -122.51073936441803),
+        "415-975-4767",
+        "NO HAMS ALLOWED"
+        ),
+    Pantry(
+        "3D",
+        "Community Fridge and Cabinet", 
+        Location("20324 Forest Ave, Castro Valley, CA 94546", 37.69752069616992, -122.06889211536156),
+        "NOT AVAILABLE",
+        "Community Organized"
+        ),
+    Pantry(#
+        "1D",#id
+        "San Francisco", #charField
+        Location("San Francisco, CA, USA", 37.77514760291072, -122.41941013558197),#charField, integerField
+        "415-975-4767",#charField 
+        "Only has ham"#textField
+        ),
+    Pantry(
+        "2D",
+        "Ocean Beach", 
+        Location("Ocean Beach, San Francisco, CA, USA", 37.75971813007669, -122.51073936441803),
+        "415-975-4767",
+        "NO HAMS ALLOWED"
+        ),
+    Pantry(
+        "3D",
+        "Community Fridge and Cabinet", 
+        Location("20324 Forest Ave, Castro Valley, CA 94546", 37.69752069616992, -122.06889211536156),
+        "NOT AVAILABLE",
+        "Community Organized"
+        ),
+
+]
